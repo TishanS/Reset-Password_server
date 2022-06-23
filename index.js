6e4966909b9f2f0cfd48441e70ba42e7f83bb8d9
TishanS <94636881+TishanS@users.noreply.github.com>
2022-06-23 08:48:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,11 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/password-reset", passwordResetRoutes);
 
+app.use('/', (req,res,next) => {
+    console.log("Dashboard");
+    res.send("hi heroku");
+    next();
+})
+
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
